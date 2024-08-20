827b2efe2a35221fe4635e5c86be4b0c68573335
e-lawniczak <eryk.lawniczak@gmail.com>
2024-08-20 13:13:38 +0200

    Updates

--- assets/AboutPage-BzbwVhnP.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-lSTcoWcA.js";import{B as e}from"./BasePage-DpNPq40a.js";const t=()=>s.jsx(e,{pageCssClass:"about-page",children:s.jsx("div",{className:"wrapper",children:s.jsx("h1",{children:"About me"})})});export{t as default};
