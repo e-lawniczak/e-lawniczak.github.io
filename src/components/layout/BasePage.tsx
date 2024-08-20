8dd2bb7d9c9dfffb40e44f279cefc5fb1d93ec25
e-lawniczak <eryk.lawniczak@gmail.com>
2024-08-20 13:27:33 +0200

    Update BasePage.tsx

--- src/components/layout/BasePage.tsx
+++ src/components/layout/BasePage.tsx
@@ -1,18 +1,11 @@
-import { useEffect } from "react";
 import { Footer } from "./Footer"
 import { Header } from "./Header"
-import { useSearchParams } from "react-router-dom";
 
 export const BasePage = (props: { children: any, pageCssClass: string, [x: string]: any }) => {
     const
-        { children, pageCssClass } = props,
-        [searchParams] = useSearchParams();
+        { children, pageCssClass } = props
 
 
-    useEffect(() => {
-        console.log(searchParams);
-        console.log(searchParams.get("xd"));
-    }, [])
     return <div className={["page", pageCssClass].join(" ")}>
         <Header />
         <div className="page-content">
