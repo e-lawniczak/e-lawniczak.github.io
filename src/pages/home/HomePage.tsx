21d54f40e73548a83729ab693acda3a352a720a7
e-lawniczak <60924751+e-lawniczak@users.noreply.github.com>
2025-02-19 01:20:42 +0100

    quick links

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom"
 import { PageTitle } from "../../components/common/PageTitle"
 import { BasePage } from "../../components/layout/BasePage"
 import './_home.scss'
@@ -5,6 +6,12 @@ export default () => {
     return <BasePage pageCssClass="home-page">
         <div className="wrapper">
             <PageTitle>About</PageTitle>
+            <div className="quick-links">
+                <h3>Important stuff:</h3>
+                <NavLink to={"/Experience"}>Expierience</NavLink>
+                <NavLink to={"/Skills"}>Skills</NavLink>
+                <NavLink to={"/Projects"}>Projects</NavLink>
+            </div>
             <p>
                 Hey! I'm Eryk, a passionate <b>software developer</b>, uni grad with <b>master's degree</b> in computer science complete with IT technichan education from technical college.
             </p>
@@ -15,7 +22,7 @@ export default () => {
                 During my uni I was working part-time as a fullstack developer at Thinq. Working with technologies like <b>.NET</b>, <b>ASP.NET</b>, <b>React</b>, <b>Azure Dev Ops</b>, <b>Git</b> and many more.
             </p>
             <p>
-                I never stop learning and I take every opportunity to develop my skills wether they are technical or soft skills. I started weekend part-time studies with psychology as my major.   
+                I never stop learning and I take every opportunity to develop my skills wether they are technical or soft skills. I started weekend part-time studies with psychology as my major.
             </p>
             <p>
                 With my hollistic education I bring more than just a bootcamp knowledge restricted to one technology and programming language. I treat technologies as tools to get the job done. Strong problem-solving and algorithmic thinking skills are the backbone of my day to day work.
